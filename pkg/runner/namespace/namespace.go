2c4d6a5bab43c8c85d3c8fd85b227898f9d8e558
Dallas Kaman <dallas.kaman@praetorian.com>
2021-09-23 15:20:51 -0500

    envoy strategy for namespace runner

--- pkg/runner/namespace/namespace.go
+++ pkg/runner/namespace/namespace.go
@@ -2,7 +2,9 @@ package namespace
 
 import (
 	"fmt"
+	"regexp"
 
+	"github.com/praetorian-inc/mithril/pkg/envoy"
 	"github.com/praetorian-inc/mithril/pkg/runner"
 )
 
@@ -36,9 +38,26 @@ func (s *DefaultStrategy) Verify(input map[string]string) error {
 type EnvoyStrategy struct{}
 
 func (s *EnvoyStrategy) Run(input map[string]string) (map[string]string, error) {
-	// istiod.istio-system.svc.cluster.local:15010
+	// curl -s 127.0.0.1:15000/server_info | jq -r .node.metadata.PROXY_CONFIG.discoveryAddress
+	ec, err := envoy.RetrieveConfig("http://localhost:15000/config_dump")
+	if err != nil {
+		return nil, err
+	}
+
+	address, err := ec.DiscoveryAddress()
+	if err != nil {
+		return nil, err
+	}
+
+	r := regexp.MustCompile(`istiod\.(.*)\.svc.*:.*`)
+
+	match := r.FindStringSubmatch(address)
+	if match == nil {
+		return nil, fmt.Errorf("%s did not match expected regex of `istiod.<namespace>.svc:<port>`", address)
+	}
+
 	return map[string]string{
-		runner.IstioNamespaceKey: "istio-system",
+		runner.IstioNamespaceKey: match[1],
 	}, nil
 }
 

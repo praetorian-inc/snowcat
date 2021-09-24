646eca8cff962a743abd495c7c198f63d7c29f2b
Anthony Weems <amlweems@gmail.com>
2021-09-24 09:54:38 -0500

    add runners to main

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/praetorian-inc/mithril/auditors"
 	"github.com/praetorian-inc/mithril/pkg/context/xds"
+	"github.com/praetorian-inc/mithril/pkg/runner"
+	"github.com/praetorian-inc/mithril/pkg/runner/discovery"
+	"github.com/praetorian-inc/mithril/pkg/runner/namespace"
 	"github.com/praetorian-inc/mithril/pkg/types"
 
 	_ "github.com/praetorian-inc/mithril/auditors/auth"
@@ -19,11 +22,28 @@ func main() {
 		log.Fatalf("failed to initialize auditors: %s", err)
 	}
 
+	runners := []runner.Runner{
+		namespace.Runner,
+		discovery.Runner,
+	}
+	conf := make(map[string]string)
+	for _, r := range runners {
+		err := r.Run(conf)
+		if err != nil {
+			log.Fatalf("failed to run %T: %s", r, err)
+		}
+	}
+
+	addr, ok := conf[runner.DiscoveryAddressKey]
+	if !ok {
+		log.Fatalf("unable to discover %s", runner.DiscoveryAddressKey)
+	}
+
 	// ctx, err := static.New("_fixtures/")
 	// if err != nil {
 	// 	log.Fatalf("failed to initialize context: %s", err)
 	// }
-	ctx, err := xds.New("127.0.0.1:15010")
+	ctx, err := xds.New(addr)
 	if err != nil {
 		log.Fatalf("failed to initialize context: %s", err)
 	}

327532ab164571297365d052bd23cbbfe543359b
Anthony Weems <amlweems@gmail.com>
2021-09-28 12:03:20 -0500

    initial commit of kubelet client

---
+++ pkg/kubelet/client.go
@@ -0,0 +1,73 @@
+package kubelet
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+
+	istioscheme "istio.io/client-go/pkg/clientset/versioned/scheme"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientsetscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+)
+
+type KubeletClient interface {
+	Pods(ctx context.Context) ([]v1.Pod, error)
+}
+
+type kubeletClient struct {
+	kubeletAddr string
+
+	decoder runtime.Decoder
+}
+
+// NewClient creates an XDS client given a GRPC address.
+func NewClient(addr string) (KubeletClient, error) {
+	err := istioscheme.AddToScheme(clientsetscheme.Scheme)
+	if err != nil {
+		return nil, err
+	}
+
+	cli := &kubeletClient{
+		kubeletAddr: addr,
+		decoder:     clientsetscheme.Codecs.UniversalDeserializer(),
+	}
+	return cli, cli.verify()
+}
+
+func (c *kubeletClient) verify() error {
+	url := fmt.Sprintf("http://%s/healthz/ping", c.kubeletAddr)
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("status code %d from %s", resp.StatusCode, url)
+	}
+	return nil
+}
+
+// Pods queries the read-only kubelet API for a list of pods running on that node.
+func (c *kubeletClient) Pods(ctx context.Context) ([]v1.Pod, error) {
+	resp, err := http.Get(fmt.Sprintf("http://%s/pods", c.kubeletAddr))
+	if err != nil {
+		return nil, err
+	}
+
+	buf, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	obj, _, err := c.decoder.Decode(buf, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	pods, ok := obj.(*v1.PodList)
+	if !ok {
+		return nil, fmt.Errorf("unexpected type %T from kubelet", obj)
+	}
+	return pods.Items, nil
+}
